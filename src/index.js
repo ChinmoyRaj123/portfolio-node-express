ea3427573291daa4cbddbd6a0fe42f6542896878
Chinmoy Raj Deka <rajchinmoy007@gmail.com>
2023-02-08 11:07:53 +0530

    view engine setup using hbs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const path = require("path")
 const app = express()
 const port = 4000
 
+app.set("view engine", "hbs")
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  res.render("index", { name: "Chinmoy", age: 28 })
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/about.html'))
+  res.render("about", { sex: "Male", height: "5.10" })
 })
 
 app.listen(port, () => {
